9950d5f224982a8c8109d92ab1685b082d914a86
Sean Reilly <sean@hatchet.run>
2024-11-19 13:39:45 -0800

    clean run from start to finish

--- terraform-stack/deploy/environments/benchmarks/benchmarks/secrets.tf
+++
@@ -1,8 +0,0 @@
-
-
-data "google_secret_manager_secret_version" "rabbitmq_password" {
-  project = "hatchet-benchmarks"
-  secret = "benchmarks-rabbitmq-password"
-}
-
-#
--- terraform-stack/modules/gcp/vpc/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "vpc_name" {
-  value = google_compute_network.vpc.name
-}
-
-output "subnet_name" {
-  value = google_compute_subnetwork.subnet.name
-}
